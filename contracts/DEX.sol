14869a98a78b56653c2afa7c0e38356e261ec60a
Alexander Treder <alexander.treder@bertelsmann.de>
2022-03-11 11:43:08 +0100

    simple dex backend

---
+++ contracts/DEX.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+import './ERC20Basic.sol';
+
+contract DEX {
+    ERC20Basic public token;
+
+    event Bought(uint256 amount);
+    event Sold(uint256 amount);
+
+    constructor() {
+        token = new ERC20Basic(1000 * 10 ** 18);
+    }
+
+    function buy() payable public {
+        uint256 amountTobuy = msg.value;
+        uint256 dexBalance = token.balanceOf(address(this));
+        require(amountTobuy > 0, "You need to send some Ether");
+        require(amountTobuy <= dexBalance, "Not enough tokens in the reserve");
+        token.transfer(msg.sender, amountTobuy);
+        emit Bought(amountTobuy);
+    }
+
+    function sell(uint256 amount) public {
+        require(amount > 0, "You need to sell at least some tokens");
+        uint256 allowance = token.allowance(msg.sender, address(this));
+        require(allowance >= amount, "Check the token allowance");
+        token.transferFrom(msg.sender, address(this), amount);
+        payable(msg.sender).transfer(amount);
+        emit Sold(amount);
+    }
+}
